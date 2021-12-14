7a4416b50b7144491a072704c60a7143e2c24eb0
Adam Souza <adamrsouza@hotmail.com>
2021-12-13 20:06:40 -0400

    adicionada view de equipe

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -17,9 +17,20 @@ namespace e_jogos.Controllers
             return View();
         }
 
-        //public IActionResult Cadastrar(IFormCollection form)
-        //{
-             
-        //}
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //Criando um objeto equipe a partir do frontend (IFormCollection)
+            Equipe nova_equipe = new Equipe();
+            nova_equipe.IdEquipe = int.Parse(form["IdEquipe"]);
+            nova_equipe.Nome = form["Nome"];
+            nova_equipe.Imagem = form["Imagem"];
+
+            //Chamando a função create do objeto passando a nova equipe
+            equipeModel.Create(nova_equipe);
+            
+            ViewBag.Equipes = equipeModel.ReadAll();
+
+            return LocalRedirect("~/Equipe");
+        }
     }
 }
